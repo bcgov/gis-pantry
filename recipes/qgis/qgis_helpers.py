283131f09a3dad3f86f5be665e9cc65f7644820e
Burt <William.Burt@gov.bc.ca>
2020-04-07 16:23:52 -0700

    qgis python helpers
    new qgis_helpers.py - a library of reusable helper functions
    update qgis_set_environment.py

---
+++ recipes/qgis/qgis_helpers.py
@@ -0,0 +1,161 @@
+# qgis_helpers.py
+# description: some helpers for qgis standalone python scripts
+import sys
+import os
+import qgis_set_environment
+from osgeo import ogr
+from qgis.PyQt.QtSql import QSqlDatabase, QSqlQuery 
+
+def create_oracle_layer(layer_name,user_name,user_pass,db_table,geom_column_name,sql=None,geom_type=None,key='OBJECTID'):
+    # create an QgsVector from oracle table
+    uri = QgsDataSourceUri()
+    uri.setConnection('bcgw.bcgov', '1521','idwprod1.bcgov', user_name, user_pass)
+    schema, table = db_table.split('.')
+    geom_c = get_bcgw_geomcolumn(db_table=db_table,user_name=user_name,user_pass=user_pass)
+    geom_type = get_bcgw_table_geomtype(db_table=db_table,geom_column_name=geom_c,user_name=user_name,user_pass=user_pass)
+    key = get_bcgw_column_key(db_table=db_table,user_name=user_name,user_pass=user_pass)
+    if sql is not None:
+        if len(sql)>0:
+            if 'WHERE' in sql.upper():
+                uri.setSql(sql)
+                uri.setDataSource(schema, table, geom_column_name)
+            else:
+                table = f"(select * from {db_table} WHERE {sql} )"
+                uri.setDataSource("", table, geom_column_name,"",key)
+    uri.setSrid('EPSG:3005')
+    
+    uri.setUseEstimatedMetadata(True)
+    uri.setKeyColumn(key)
+    
+    if geom_type == 'Point':
+        uri.setWkbType(QgsWkbTypes.Point)
+    elif geom_type == 'Polygon':
+        uri.setWkbType(QgsWkbTypes.Polygon)
+    elif geom_type == 'LineString':
+        uri.setWkbType(QgsWkbTypes.LineString)
+    elif geom_type == 'MultiPolygon':
+        uri.setWkbType(QgsWkbTypes.MultiPolygon)
+    elif geom_type == 'MultiLine':
+        uri.setWkbType(QgsWkbTypes.MultiLineString)
+    elif geom_type == 'MulitPoint':
+        uri.setWkbType(QgsWkbTypes.MulitPoint)
+    else:
+        print (f"Unexpected geometry: {geom_type}")
+
+    tlayer = QgsVectorLayer(uri.uri(), layer_name, 'oracle')
+    assert tlayer.isValid()
+    tlayer.setCrs(QgsCoordinateReferenceSystem("EPSG:3005"))
+    
+    return tlayer
+
+def get_bcgw_table_geomtype(db_table,geom_column_name,user_name,user_pass):
+    # get geometry type from oracle table - oracle stores multiple types so
+    # this returns the maximum type ie multiline, multipolygon, multipoint if
+    # present in geometry
+    owner,table = db_table.split('.') 
+    driver ="QOCISPATIAL"
+    conn_name = "bcgw_conn"
+    if not QSqlDatabase.contains(conn_name):
+        db = QSqlDatabase.addDatabase(driver,conn_name)
+    else:
+        db = QSqlDatabase.database(conn_name)
+    db.setDatabaseName('bcgw.bcgov' + "/" + 'idwprod1.bcgov') 
+    db.setUserName(user_name) 
+    db.setPassword(user_pass) 
+    db.open()
+    if not db.open(): 
+        print ("Failed Connection from find_bcgw_the_geom") 
+    q = QSqlQuery(db) 
+    query = f"SELECT MAX(t.{geom_column_name}.GET_GTYPE()) AS geometry_type from {owner}.{table} t"
+    q.exec(query) 
+    q.first()
+    type_num = q.value(0)
+    if type_num == 1:
+        geom_t = 'Point'
+    elif type_num == 2:
+        geom_t = 'LineString'
+    elif type_num == 3:
+        geom_t = 'Polygon'
+    elif type_num == 7:
+        geom_t = 'MultiPolygon'
+    elif type_num ==5:
+        geom_t = 'MulitPoint'
+    elif type_num ==6:
+        geom_t = 'MultiLineString '
+    else:
+        db.close()
+        raise TypeError
+    db.close()
+    return geom_t
+
+def get_bcgw_geomcolumn(db_table,user_name,user_pass):
+    # get the name of the geometry column for oracle table
+    owner,table = db_table.split('.') 
+    driver ="QOCISPATIAL" 
+    conn_name = "bcgw_conn"
+    if not QSqlDatabase.contains(conn_name):
+        db = QSqlDatabase.addDatabase(driver,conn_name)
+    else:
+        db = QSqlDatabase.database(conn_name)
+    db.setDatabaseName('bcgw.bcgov' + "/" + 'idwprod1.bcgov') 
+    db.setUserName(user_name) 
+    db.setPassword(user_pass) 
+    db.open()
+    if not db.open(): 
+        print ("Failed Connection from find_bcgw_the_geom") 
+    q = QSqlQuery(db) 
+    query ="SELECT COLUMN_NAME from all_tab_columns where OWNER = '{}' AND TABLE_NAME = '{}' AND DATA_TYPE = 'SDO_GEOMETRY'".format(owner,table)  
+    q.exec(query) 
+    q.first() 
+    geom_c = q.value(0)
+    db.close()
+    return geom_c
+
+def get_bcgw_column_key(db_table,user_name,user_pass):
+    # estimate a unique id column for an oracle table if OBJECTID does not exist
+    owner,table = db_table.split('.') 
+    driver ="QOCISPATIAL" 
+    conn_name = "bcgw_conn"
+    if not QSqlDatabase.contains(conn_name):
+        db = QSqlDatabase.addDatabase(driver,conn_name)
+    else:
+        db = QSqlDatabase.database(conn_name)
+    db.setDatabaseName('bcgw.bcgov' + "/" + 'idwprod1.bcgov') 
+    db.setUserName(user_name) 
+    db.setPassword(user_pass) 
+    db.open()
+    if not db.open(): 
+        print ("Failed Connection from find_bcgw_the_geom") 
+    q = QSqlQuery(db)
+    sql = f"SELECT cols.column_name \
+    FROM all_tab_cols cols where cols.table_name = '{table}' and cols.COLUMN_NAME like \'OBJECTID\'"
+    q.exec(sql)
+    if q.first():
+        key_c = q.value(0)
+    else:
+        sql = f"SELECT COLUMN_NAME FROM all_tab_cols where table_name = '{table}' \
+            order by COLUMN_ID FETCH FIRST 1 ROWS ONLY"
+        q.exec(sql)
+        if q.first():
+            key_c = q.value(0)
+    db.close()
+    return key_c
+
+def create_gpkg(root,name,overwrite=False):
+    # create an empty geopackage
+    fullpath = os.path.join(root,name)
+    if os.path.exists(fullpath):
+        if overwrite is True:
+            try:
+                os.remove(fullpath)
+            except OSError as error:
+                print (error)
+                print (f"Error removing: {fullpath}")
+        else:
+            print (f"Error: File exists (overwite=False)")
+            return None
+    else:
+        driver = ogr.GetDriverByName('GPKG')
+        driver.CreateDataSource(fullpath)
+        return fullpath
+
--- recipes/qgis/qgis_set_environment.py
+++ recipes/qgis/qgis_set_environment.py
@@ -6,31 +6,36 @@ AUTHOR: Mark McGirr
 Date  : 06-05-2019
 Arguments: None
 Outputs: None
-Dependancies: 
-
+Dependancies: need QGIS_PATH in as environment variable
 History:
 ----------------------------------------------
 0?-05-2019 - Mark McGirr
     Creation of environment setup for qgis standalone scripts
 06-05-2019 - Will Burt
     addition of qgis_root for install path substitutions
+04-07-2020 - Will Burt
+    move qgis_root to environment variable
 ''' 
 ###############################################
-# Set your root QGIS install path
-qgis_root = 'E:/sw_nt/QGIS_3.4'
+if 'QGIS_PATH' in os.environ.keys():
+    qgis_root = os.environ['QGIS_PATH']
+else:
+    print("qgis_root not found; exiting script.")
+    sys.exit()
+
+print("qgis_root is: {}\n".format(qgis_root))
 
 # Define plugin locations from QGIS3
 sys.path.append(qgis_root + '/apps/qgis/python')
 sys.path.append(qgis_root + '/apps/qgis/plugins')
 sys.path.append(qgis_root + '/apps/qt5/bin')
-
+sys.path.append(qgis_root + '/apps/qgis/bin')
+sys.path.append(qgis_root + '/apps/qgis/python/plugins')
 
 # Define Qt5 plugin path since Qt5 can't find it
-os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = qgis_root + \
-    '/apps/Qt5/plugins'  # ;'+qgis_root + '/apps/qgis/qtplugins
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = qgis_root + '/apps/Qt5/plugins'  # ;'+qgis_root + '/apps/qgis/qtplugins
 
-
-# enviro setup from python-qgis.bat
+# Enviro setup from python-qgis.bat
 os.environ['GDAL_DATA'] = qgis_root + '/share/gdal'
 os.environ['GDAL_DRIVER_PATH'] = qgis_root + '/bin/gdalplugins'
 os.environ['GDAL_FILENAME_IS_UTF8'] = 'YES'
@@ -41,9 +46,9 @@ os.environ['QT_PLUGIN_PATH'] = qgis_root + \
 os.environ['VSI_CACHE'] = 'TRUE'
 os.environ['VSI_CACHE_SIZE'] = '1000000'
 
-# enviro setup from qt5_env.bat
+# Enviro setup from qt5_env.bat
 os.environ['QT_PLUGIN_PATH'] = qgis_root + \
-    '/apps/qgis/qtplugin;'+qgis_root + '/apps/qt5/plugins'
+    '/apps/qgis/qtplugins;'+qgis_root + '/apps/qt5/plugins'
 os.environ['O4W_QT_PREFIX'] = qgis_root + '/apps/Qt5'
 os.environ['O4W_QT_BINARIES'] = qgis_root + '/apps/Qt5/bin'
 os.environ['O4W_QT_PLUGINS'] = qgis_root + '/apps/Qt5/plugins'
@@ -52,13 +57,43 @@ os.environ['O4W_QT_TRANSLATIONS'] = qgis_root + '/apps/Qt5/translations'
 os.environ['O4W_QT_HEADERS'] = qgis_root + '/apps/Qt5/include'
 os.environ['O4W_QT_DOC'] = qgis_root + '/apps/Qt5/doc'
 
-# enviro setup from py3_env.bat
+# Enviro setup from py3_env.bat
 os.environ['PYTHONPATH'] = qgis_root + '/apps/qgis/python;'
 os.environ['PYTHONHOME'] = qgis_root + '/apps/Python37'
 os.environ['OSGEO4W_ROOT'] = qgis_root + ''
 
-
-# mimic path from cmd window after running bat file
+# Mimic path from cmd window after running .bat file
 os.environ['Path'] = qgis_root + '/apps/qgis/bin;'+qgis_root + '/apps/Python37;'+qgis_root + '/apps/Python37/Scripts;'+qgis_root + \
     '/apps/qt5/bin;'+qgis_root + '/apps/Python27/Scripts;'+qgis_root + \
     '/bin;C:/Windows/system32;C:/Windows;C:/Windows/system32/WBem'
+
+print("\nBoilerplate env OK\n")
+
+# Third party imports
+from qgis.analysis import QgsNativeAlgorithms # Possible translation: from qgis.py import <class analysis> import <class QgsNativeAlgorithms>
+print("from qgis.analysis import QgsNativeAlgorithms... SUCCESS")
+from qgis.core import *
+print("from qgis.core import *... SUCCESS")
+from processing.core.Processing import Processing
+print("from processing.core.Processing import Processing... SUCCESS")
+import processing
+print("import processing... SUCCESS")
+from PyQt5.QtCore import *
+print("from PyQt5.QtCore import... SUCCESS")
+from processing.tools import dataobjects                       # NEW - GMA added
+print("from processing.tools import dataobjects... SUCCESS")    # NEW - GMA added
+print("All third-party imports OK.")
+
+# Initialize QGIS
+feedback = QgsProcessingFeedback()
+qgs = QgsApplication([], False)
+qgs.initQgis()
+# QgsApplication.setPrefixPath(r'E:/sw_nt/QGIS_3.4\apps\qgis', True)      # Extra
+QgsApplication.setPrefixPath(qgis_root + '/apps/qgis', True)      # Extra
+QgsApplication.processingRegistry().addProvider(QgsNativeAlgorithms())
+Processing.initialize()
+
+print(f"Boilerplate ran OK: {sys.argv[0]}")
+#===============================================================================
+# End of boilerplate
+#===============================================================================
