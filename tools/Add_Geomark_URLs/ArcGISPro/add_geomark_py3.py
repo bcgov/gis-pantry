74dbc23a3847f3f74d2a8bbe507b369a42447886
Jeff Kruys <146253146+jkruys-bcgov@users.noreply.github.com>
2024-11-04 14:14:32 -0800

    20241104 update add_geomark_py3.py
    Updated script to use http.client module instead of requests module to make API calls. requests module has failed connection issues when run from within a GTS session.

--- tools/Add_Geomark_URLs/ArcGISPro/add_geomark_py3.py
+++ tools/Add_Geomark_URLs/ArcGISPro/add_geomark_py3.py
@@ -25,9 +25,16 @@ X:\fullpath\add_geomark_py3.py Y:\fullpath\lyr
 
 History
 2024-08-12 (JK): Created script.
+2024-11-04 (JK): Updated to use http.client instead of requests module to make 
+                 API calls; requests module seems to have issues when run in a 
+                 GTS session.
 """
 
-import arcpy, requests, time
+import arcpy
+import http.client
+import urllib
+import time
+import json
 
 def add_geomark_url(in_fc):
 
@@ -43,35 +50,59 @@ def add_geomark_url(in_fc):
     else:
         arcpy.AddMessage(time.strftime('%Y-%m-%d %H:%M:%S : ') + "Geomark_URL field already exists in input layer")
 
-    # Read each record, send request to create Geomark URL, write the returned URL to the Geomark_URL field
+    # Read each record, send API request to create Geomark URL, write the returned URL to the Geomark_URL field
     row_total = int(arcpy.GetCount_management(in_fc).getOutput(0))
     read_count = 0
     update_count = 0
     arcpy.AddMessage(time.strftime('%Y-%m-%d %H:%M:%S : ') + f"Processing {row_total} feature(s) of input dataset")
-    session = requests.Session()
     with arcpy.da.UpdateCursor(in_fc, ["Geomark_URL", "SHAPE@"]) as cursor:
         for row in cursor:
             read_count += 1
             exist_url = row[0]
-            if exist_url is None:
+            if exist_url is None or exist_url.startswith("Geomark Web Service request failed"):
                 geom = row[1]
                 if geom is None:
                     row[0] = "Null geometry"
                 else:
+                    
+                    # Construct a POST request via http.client module - it works better than requests module when
+                    # sent from a GTS session, and should work for anyone outside GTS too
+                    conn = http.client.HTTPSConnection("apps.gov.bc.ca")
+
+                    # payload string should have been easy to build with the following code:
+                    # payload_dict = {"bufferSegments": 8, "body": geom_wkt, "srid": srid, "bufferMetres": None, etc. }
+                    # payload = urllib.parse.urlencode(payload_dict)
+
+                    # But I could not get this working. Instead I'm only urlencode'ing the geometry and srid, and 
+                    # hardcoding the rest of the string as follows, and it seems to work:
                     geom_wkt = geom.WKT
-                    post_url = "https://apps.gov.bc.ca/pub/geomark/geomarks/new"
-                    payload = {"bufferSegments": 8, "body": geom_wkt, "bufferMetres": None, "callback": None, 
-                               "failureRedirectUrl": None, "bufferJoin": "ROUND", "bufferMitreLimit": 5, 
-                               "bufferCap": "ROUND", "redirectUrl": None, "resultFormat": None, "format": "wkt", 
-                               "srid": srid, "allowOverlap": "false"}
-                    r = session.post(post_url, data=payload, timeout=5)
-                    if r.status_code == 200:
-                        out_url = r.url
-                        if out_url == "https://apps.gov.bc.ca/pub/geomark/geomarks/new":
-                            row[0] = "Could not create Geomark URL for this geometry"
+                    geom_dict = {"body": geom_wkt, "srid": srid}
+                    geom_dict_urle = urllib.parse.urlencode(geom_dict)
+                    payload = fr"bufferSegments=8&{geom_dict_urle}&bufferMetres=&callback=&failureRedirectUrl="
+                    payload += fr"&bufferJoin=ROUND&bufferMitreLimit=5&bufferCap=ROUND&redirectUrl=&resultFormat="
+                    payload += fr"&format=wkt&allowOverlap=false"
+                    headers = { 'content-type': "application/x-www-form-urlencoded" }
+                    conn.request("POST", "/pub/geomark/geomarks/new", payload, headers)
+                    res = conn.getresponse().read().decode()
+                    res_received = True
+                    try:
+                        res_dict = json.loads(res.replace(";", "").replace("(", "").replace(")", ""))
+                    except:
+                        res_received = False
+                        row[0] = "Geomark Web Service request failed: no response received"
+                    if res_received:
+                        if isinstance(res_dict, dict):
+                            if "url" in res_dict:
+                                if res_dict["url"].startswith("https://apps.gov.bc.ca/pub/geomark/geomarks/gm-"):
+                                    row[0] = res_dict["url"]
+                                    update_count += 1
+                                else:
+                                    row[0] = "Geomark Web Service request failed: URL in response is not a Geomark"
+                            else:
+                                row[0] = "Geomark Web Service request failed: response did not contain a URL"
                         else:
-                            row[0] = out_url
-                            update_count += 1
+                            row[0] = "Geomark Web Service request failed: response could not be loaded to a JSON object"
+                    del conn
                 cursor.updateRow(row)
 
             if read_count % 10 == 0 or read_count == row_total:
